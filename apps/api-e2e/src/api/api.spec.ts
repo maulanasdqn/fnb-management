1c391c4f9ee8ca630ab57010b3b80ad9b6a3edfe
sugiartofahmi <sugiartofahmi@gmail.com>
2024-03-17 17:02:47 +0700

    chore: initial api

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 describe('GET /', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/`);
+    const res = await axios.get(`/api`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toEqual({ message: 'Welcome to api!' });
   });
 });
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
