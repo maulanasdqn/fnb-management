f7da705a64e91506fbde2f5d4ab8ef8e4a43bd86
sugiartofahmi <sugiartofahmi@gmail.com>
2024-03-18 04:30:48 +0700

    improvement: add cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { createContext, trpcController } from '@fms/api';
+import cors from 'cors';
 const app = express();
 
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(
   '/trpc',
   trpcExpress.createExpressMiddleware({
