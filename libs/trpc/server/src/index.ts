06006e9083fcbb0bdcb4f0c30720b232b6098d08
Maulana Sodiqin <maulanasdqn@gmail.com>
2024-03-27 01:30:45 +0700

    fix: server wont running beacuse superjson wont support commonjs

--- libs/trpc/server/src/index.ts
+++ libs/trpc/server/src/index.ts
@@ -1,15 +1,12 @@
-import * as trpcExpress from '@trpc/server/adapters/express';
 import { initTRPC } from '@trpc/server';
 import superjson from 'superjson';
-export const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({});
+export const createContext = () => ({});
 
 type TrpcContext = Awaited<ReturnType<typeof createContext>>;
 
 const t = initTRPC.context<TrpcContext>().create({
   transformer: superjson,
 });
+
 export const router = t.router;
 export const procedure = t.procedure;
