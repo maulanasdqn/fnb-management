ea5c1cecd5a5b007bd371ab6ca9b02ca8e2b33de
rafli <rafliforbusiness@gmail.com>
2024-04-04 23:59:54 +0700

    Mochamad Rafli | improvment update item logs when items change

--- libs/api/src/item/services/item.service.ts
+++ libs/api/src/item/services/item.service.ts
@@ -1,4 +1,4 @@
-import { db, items } from '@fms/drizzle';
+import { db, items, itemLogs } from '@fms/drizzle';
 import {
   TItemQueryParams,
   TItemCreateRequest,
@@ -6,7 +6,7 @@ import {
   TItemResponse,
   TItemSingleResponse,
 } from '@fms/entities';
-import { ilike, asc, eq } from 'drizzle-orm';
+import { ilike, asc, eq, desc } from 'drizzle-orm';
 
 export const findMany = async (
   params?: TItemQueryParams
@@ -51,47 +51,110 @@ export const create = async ({
   itemAmount,
   itemAmountTypeId,
   ingredientUnit,
+  quantity,
   ingredientAmount,
 }: TItemCreateRequest): Promise<TItemSingleResponse> => {
-  const data = await db
-    .insert(items)
-    .values({
-      name,
-      price,
-      itemAmount,
-      itemAmountTypeId,
-      ingredientUnit,
-      ingredientAmount,
-    })
-    .returning();
-  console.log("service", data)
 
-  return data[0] as TItemSingleResponse;
+  const returnData: TItemSingleResponse = await db.transaction(
+    async (tx) => {
+      const data = await tx
+        .insert(items)
+        .values({
+          name,
+          price,
+          itemAmount,
+          itemAmountTypeId,
+          ingredientUnit,
+          ingredientAmount,
+        })
+        .returning();
+
+      if (!data[0].id) {
+        return data[0] as TItemSingleResponse;
+      }
+
+      await tx
+        .insert(itemLogs)
+        .values({
+          itemId: data[0].id,
+          qtyBefore: '0',
+          qtyCurrent: quantity ? quantity.toString() : '0',
+          qtyAfter: quantity ? quantity.toString() : '0',
+        })
+        .returning();
+
+      return data[0] as TItemSingleResponse;
+    }
+  )
+  return returnData as TItemSingleResponse;
 };
 
 export const update = async ({
   id,
   name,
   price,
+  quantity,
   itemAmount,
   itemAmountTypeId,
   ingredientUnit,
   ingredientAmount,
 }: TItemUpdateRequest) => {
-  const data = await db
-    .update(items)
-    .set({
-      name,
-      price,
-      itemAmount,
-      itemAmountTypeId,
-      ingredientUnit,
-      ingredientAmount,
-    })
-    .where(eq(items.id, id))
-    .returning();
+  const returnData: TItemSingleResponse = await db.transaction(
+    async (tx) => {
+      const data = await tx
+        .update(items)
+        .set({
+          name,
+          price,
+          itemAmount,
+          itemAmountTypeId,
+          ingredientUnit,
+          ingredientAmount,
+        })
+        .where(eq(items.id, id))
+        .returning();
 
-  return data[0] as TItemSingleResponse;
+      if (!data[0].id) {
+        return data[0] as TItemSingleResponse;
+      }
+
+      let qtyBefore = 0
+      let qtyAfter = quantity
+
+      const logs = await tx
+        .select({
+          qtyAfter: itemLogs.qtyAfter,
+        })
+        .from(itemLogs)
+        .where(eq(itemLogs.itemId, id))
+        .orderBy(desc(
+          itemLogs.createdAt
+        ));
+
+      if (logs.length > 0) {
+        qtyBefore = Number(logs[0].qtyAfter);
+        qtyAfter = quantity
+          ? Number(logs[0].qtyAfter) + Number(quantity)
+          : Number(logs[0].qtyAfter);
+      }
+
+      if (quantity !== qtyBefore) {
+        await tx
+          .insert(itemLogs)
+          .values({
+            itemId: data[0].id,
+            qtyBefore: qtyBefore.toString(),
+            qtyCurrent: quantity ? quantity.toString() : '0',
+            qtyAfter: qtyAfter.toString(),
+          })
+          .returning();
+
+      }
+
+      return data[0] as TItemSingleResponse;
+    }
+  )
+  return returnData as TItemSingleResponse;
 };
 
 export const destroy = async (id: string) => {
--- libs/entities/src/validation-schemas/item/item.schema.ts
+++ libs/entities/src/validation-schemas/item/item.schema.ts
@@ -24,6 +24,7 @@ export const itemCreateRequestSchema = z.object({
   itemAmount: z.string().nullable(),
   ingredientUnit: z.enum(['ml', 'g']).nullable(),
   ingredientAmount: z.string().nullable(),
+  quantity: z.number().default(0),
   ...baseSchema.omit({ id: true }).shape,
 });
 
@@ -31,6 +32,7 @@ export const itemUpdateRequestSchema = z.object({
   id: z.string(),
   name: z.string(),
   price: z.number(),
+  quantity: z.number(),
   itemAmountTypeId: z.string(),
   itemAmount: z.string().nullable(),
   ingredientUnit: z.enum(['ml', 'g']).nullable(),
