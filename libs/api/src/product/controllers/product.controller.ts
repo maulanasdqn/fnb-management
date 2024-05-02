5cbae21183a6879a19870ad6c61c77f54ef60686
sugiartofahmi <sugiartofahmi@gmail.com>
2024-05-02 13:59:20 +0700

    chore: remove validation output

--- libs/api/src/product/controllers/product.controller.ts
+++ libs/api/src/product/controllers/product.controller.ts
@@ -17,36 +17,29 @@ import {
 } from '@fms/entities';
 
 export const productController = router({
-  findMany: procedure
-    .output(dataResponseSchema(productResponseSchema))
-    .input(queryParamsSchema)
-    .query(async ({ input }) => {
-      return await findMany(input);
-    }),
+  findMany: procedure.input(queryParamsSchema).query(async ({ input }) => {
+    return await findMany(input);
+  }),
 
   findOne: procedure
-    .output(dataSingleResponseSchema(productResponseSchema))
     .input(productQueryParamSchema.pick({ id: true }))
     .query(async ({ input }) => {
       return await findOne(input?.id as string);
     }),
 
   create: procedure
-    .output(dataSingleResponseSchema(productResponseSchema))
     .input(productCreateRequestSchema)
     .mutation(async ({ input }) => {
       return await create(input);
     }),
 
   update: procedure
-    .output(dataSingleResponseSchema(productResponseSchema))
     .input(productUpdateRequestSchema)
     .mutation(async ({ input }) => {
       return await update(input);
     }),
 
   destroy: procedure
-    .output(dataSingleResponseSchema(productResponseSchema))
     .input(productQueryParamSchema.pick({ id: true }))
     .mutation(async ({ input }) => {
       return await destroy(input.id as string);
