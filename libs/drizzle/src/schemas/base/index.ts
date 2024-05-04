ef5827c39fa9d53962f111242f7aac3dfee731e7
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-05-05 05:20:41 +0700

    feat: add deleted on base schema

--- libs/drizzle/src/schemas/base/index.ts
+++ libs/drizzle/src/schemas/base/index.ts
@@ -4,6 +4,6 @@ export const baseSchema = {
   id: uuid('id').defaultRandom().primaryKey(),
   createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
   updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow(),
-  deletedAt: timestamp('deleted_at', { withTimezone: true }).defaultNow(),
+  deletedAt: timestamp('deleted_at', { withTimezone: true }),
   isDeleted: boolean('is_deleted').default(false),
 };
