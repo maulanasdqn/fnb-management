9402d95917ec7a58fe082a8d71626468780ffb6b
dika841 <95964808+dika841@users.noreply.github.com>
2024-05-28 23:24:53 +0700

    Improvement/page login backoffice (#44)
    * improve : update drizzle version to latest
    
    * bugFix : fix create seeder user
    
    * improvement : add message error when login not valid
    
    * improvement : add message error when login not valid

--- apps/backoffice/src/routers/login/index.tsx
+++ apps/backoffice/src/routers/login/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useEffect, useState } from 'react';
 import { Button } from '@fms/atoms';
 import { ControlledFieldText } from '@fms/organisms';
 import { useForm } from 'react-hook-form';
@@ -14,6 +14,7 @@ import { z } from 'zod';
 
 export const LoginPage: FC = (): ReactElement => {
   const [isAuthenticated, setIsAuthenticated] = useIsAuthenticated();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const {
     control,
     handleSubmit,
@@ -39,16 +40,30 @@ export const LoginPage: FC = (): ReactElement => {
           userService.setUserData(resp?.user);
         }
       },
+      onError: (error) => {
+        setErrorMessage(error.message);
+      },
     });
   });
-
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 3000);
+  }, [errorMessage]);
   return (
     <div className="bg-grey-100 flex items-center justify-center w-full h-screen text-white">
       <form
         onSubmit={onSubmit}
-        className="bg-white shadow-sm rounded-lg p-6 md:w-1/3 w-1/2 h-1/2 flex-col justify-start flex"
+        className="bg-white shadow-sm rounded-lg p-6 md:w-1/3 w-1/2 h-2/3 flex-col justify-start flex"
       >
-        <h1 className="text-black text-3xl font-medium">Login Backoffice</h1>
+       <div className="flex flex-col gap-y-2">
+          <h1 className="text-black text-3xl font-medium">Login Backoffice</h1>
+          {errorMessage && (
+            <span className="text-error-500 bg-error-50 border border-error-500 rounded-lg p-2 text-center">
+              {errorMessage}
+            </span>
+          )}
+        </div>
         <section className="flex flex-col gap-y-4 justify-center h-full">
           <ControlledFieldText
             required
@@ -68,7 +83,7 @@ export const LoginPage: FC = (): ReactElement => {
             label="Password"
             placeholder="Password"
           />
-          <Button disabled={!isValid || isPending} type="submit" size="md">
+          <Button state={isPending ? 'loading' : 'default'} disabled={!isValid || isPending} type="submit" size="md">
             Login
           </Button>
         </section>
