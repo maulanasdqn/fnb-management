dd6ceafaa341fe33f453c1c0708c96c79fb3423b
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-05 23:50:51 +0700

    improve: grouping response permission

--- libs/entities/src/validation-schemas/me/permission.schema.ts
+++ libs/entities/src/validation-schemas/me/permission.schema.ts
@@ -3,4 +3,9 @@ import { z } from 'zod';
 export const permissionResponseSchema = z.object({
   name: z.string(),
   id: z.string(),
+  parent: z.string(),
+  group: z.string(),
+  key: z.string(),
+  createdAt: z.date().nullable(),
+  updatedAt: z.date().nullable(),
 });
--- libs/entities/src/validation-schemas/me/role.schema.ts
+++ libs/entities/src/validation-schemas/me/role.schema.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod';
 
 export const roleResponseSchema = z.object({
-  name: z.string(),
   id: z.string(),
-  permissions: z.array(z.object({ name: z.string(), id: z.string() })),
+  name: z.string(),
+  createdAt: z.date().nullable(),
+  updatedAt: z.date().nullable(),
 });
