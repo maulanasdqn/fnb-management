a01deda0f477a0b11c9f1dfb7d0858f2c1d1f222
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-06 02:47:40 +0700

    feat: endpoint detail role

--- libs/api/src/me/services/role.service.ts
+++ libs/api/src/me/services/role.service.ts
@@ -1,10 +1,16 @@
-import { db } from '@fms/drizzle';
-import { TQueryParams, TResponse, roleResponseSchema } from '@fms/entities';
-import { z } from 'zod';
+import { db, roles } from '@fms/drizzle';
+import {
+  TQueryParams,
+  TResponse,
+  TRoleResponse,
+  TRoleSingleResponse,
+  roleResponseSchema,
+} from '@fms/entities';
+import { eq } from 'drizzle-orm';
 
 export const findMany = async (
   params?: TQueryParams
-): Promise<TResponse<z.infer<typeof roleResponseSchema>[]>> => {
+): Promise<TRoleResponse> => {
   const data = await db.query.roles.findMany({
     columns: {
       id: true,
@@ -13,30 +19,51 @@ export const findMany = async (
       updatedAt: true,
     },
   });
-  // const data = await db.query.roles.findMany({
-  //   columns: {
-  //     id: true,
-  //     name: true,
-  //   },
-  //   with: {
-  //     rolesToPermissions: {
-  //       with: {
-  //         permission: true,
-  //       },
-  //     },
-  //   },
-  // });
-
-  // const mappingData = data.map((role) => ({
-  //   id: role.id,
-  //   name: role.name,
-  //   permissions: role.rolesToPermissions.map((rtp) => ({
-  //     id: rtp.permission.id,
-  //     name: rtp.permission.name,
-  //   })),
-  // }));
 
   return {
     data,
+  };
+};
+
+export const findOne = async (id: string): Promise<TRoleSingleResponse> => {
+  const data = await db.query.roles.findFirst({
+    where: eq(roles.id, id),
+    columns: {
+      id: true,
+      name: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    with: {
+      rolesToPermissions: {
+        with: {
+          permission: true,
+        },
+      },
+    },
+  });
+
+  if (!data) {
+    throw Error('Role tidak ditemukan');
+  }
+
+  const permissions = data?.rolesToPermissions?.map((rtp) => ({
+    id: rtp.permission.id,
+    name: rtp.permission.name,
+    key: rtp.permission.key,
+    group: rtp.permission.group,
+    parent: rtp.permission.parent,
+    createdAt: rtp.permission.createdAt,
+    updatedAt: rtp.permission.updatedAt,
+  }));
+
+  return {
+    data: {
+      id: data.id,
+      name: data.name,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt,
+      permissions,
+    },
   };
 };
