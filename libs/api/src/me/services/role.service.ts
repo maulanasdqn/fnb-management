f3729c468875195a302daa6514353a9b77197cdc
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-07 03:27:07 +0700

    fix: type role with permission

--- libs/api/src/me/services/role.service.ts
+++ libs/api/src/me/services/role.service.ts
@@ -16,10 +16,33 @@ export const findMany = async (
       createdAt: true,
       updatedAt: true,
     },
+    with: {
+      rolesToPermissions: {
+        with: {
+          permission: true,
+        },
+      },
+    },
   });
 
+  const mappingData = data.map((role) => ({
+    id: role.id,
+    name: role.name,
+    createdAt: role.createdAt,
+    updatedAt: role.updatedAt,
+    permissions: role.rolesToPermissions.map((rtp) => ({
+      id: rtp.permission.id,
+      name: rtp.permission.name,
+      key: rtp.permission.key,
+      group: rtp.permission.group,
+      parent: rtp.permission.parent,
+      createdAt: rtp.permission.createdAt,
+      updatedAt: rtp.permission.updatedAt,
+    })),
+  }));
+
   return {
-    data,
+    data: mappingData,
   };
 };
 
