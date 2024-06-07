65720452e314a762a307f54ea499d484cfb7c573
dika841 <95964808+dika841@users.noreply.github.com>
2024-06-07 21:57:44 +0700

    Improvement/edit permission page (#55)
    * feat : crud table product
    
    * improvement : integration edit permission page
    
    * improvement : integration edit permission page

--- libs/web/components/atoms/src/inputs/input-checkbox/input-checkbox.tsx
+++ libs/web/components/atoms/src/inputs/input-checkbox/input-checkbox.tsx
@@ -9,9 +9,9 @@ export const InputCheckbox: FC<TInputSpecial> = ({
 }): ReactElement => {
   const id = useId();
   const className = clsx(
-    'focus:ring-grey-800 bg-white rounded-full',
+    'focus:ring-grey-800 bg-white rounded-full accent-primary',
     {
-      'w-4 h-4': size === 'sm',
+      'w-3 h-3': size === 'sm',
       'w-5 h-5': size === 'md',
       'w-6 h-6': size === 'lg',
     },
