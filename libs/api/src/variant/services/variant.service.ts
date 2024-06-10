64178d0a7179472db3338e9780d364ee7de9fd68
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-10 22:40:07 +0700

    chore: less code variant service

--- libs/api/src/variant/services/variant.service.ts
+++ libs/api/src/variant/services/variant.service.ts
@@ -1,42 +1,36 @@
-import {
-  db,
-  products,
-  productVariants,
-  variantOptions,
-  variants,
-} from '@fms/drizzle';
+import { db, productVariants } from '@fms/drizzle';
 import { eq } from 'drizzle-orm';
 import { TVariantOption } from '@fms/entities';
-export const findVariantByProductId = async (
-  id: string
-): Promise<TVariantOption[]> => {
-  const variantProduct = await db
-    .select({
-      id: variants.id,
-      name: variants.name,
-      option: {
-        id: variantOptions.id,
-        name: variantOptions.name,
-      },
-    })
-    .from(variants)
-    .leftJoin(variantOptions, eq(variantOptions.variantId, variants.id))
-    .leftJoin(
-      productVariants,
-      eq(productVariants.variantOptionId, variantOptions.id)
-    )
-    .leftJoin(products, eq(products.id, productVariants.productId))
-    .where(eq(productVariants.productId, id));
 
-  return Object.values(
-    variantProduct?.reduce((acc, { id, name, option }) => {
-      if (!acc[name]) {
-        acc[name] = { id, name, options: [] };
-      }
-      if (option) {
-        acc[name].options.push(option);
-      }
-      return acc;
-    }, {} as Record<string, TVariantOption>)
-  );
+export const variantService = {
+  findByProductId: async (id: string): Promise<TVariantOption[]> => {
+    const data = await db.query.variants.findMany({
+      columns: {
+        id: true,
+        name: true,
+      },
+      with: {
+        variantOptions: {
+          columns: {
+            id: true,
+            name: true,
+          },
+          with: {
+            productVariants: {
+              where: eq(productVariants.productId, id),
+            },
+          },
+        },
+      },
+    });
+
+    return data.map((el) => ({
+      id: el.id,
+      name: el.name,
+      options: el.variantOptions.map((option) => ({
+        id: option.id,
+        name: option.name,
+      })),
+    }));
+  },
 };
