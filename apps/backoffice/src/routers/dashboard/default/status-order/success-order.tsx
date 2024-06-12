4ca360f69c98a1796ed7f7815d69f052abc8feb3
dika841 <randikaa1999@gmail.com>
2024-06-12 23:20:26 +0700

    improvement: modal detail order, add modal when button is clicked

--- apps/backoffice/src/routers/dashboard/default/status-order/success-order.tsx
+++ apps/backoffice/src/routers/dashboard/default/status-order/success-order.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@fms/atoms';
+import { Modal } from '@fms/organisms';
 import { trpc } from '@fms/trpc-client';
-import { FC, ReactElement } from 'react';
-import { Link } from 'react-router-dom';
+import { capitalizeWords, formatedDate } from '@fms/utilities';
+import { FC, ReactElement, useState } from 'react';
+
 
 export const SuccessOrder: FC = (): ReactElement => {
+  const [detailModal, setDetailModal] = useState<boolean>(false);
   const data = trpc.order.findMany.useQuery();
   const filterData = data?.data?.filter((item) => item.status === 'delivered');
   return (
@@ -19,7 +22,7 @@ export const SuccessOrder: FC = (): ReactElement => {
             <p>Invoice Number :{item.invoiceNumber}</p>
             <p>
               Pesanan :{' '}
-               {item.orderDetails.map((val, index) => (
+              {item.orderDetails.map((val, index) => (
                 <span key={val.product.id}>
                   {val.product.name}
                   {index < item.orderDetails.length - 1 && ', '}
@@ -28,12 +31,58 @@ export const SuccessOrder: FC = (): ReactElement => {
             </p>
           </div>
           <div className="flex items-center">
-            <Link to="/dashboard/detail">
-              <Button size="sm">Lihat detail</Button>
-            </Link>
+            <Button size="sm" onClick={() => setDetailModal(true)}>Lihat detail</Button>
           </div>
         </div>
       ))}
+      {detailModal && (
+        <Modal
+          isOpen={detailModal}
+          onClose={() => setDetailModal(false)}
+          header
+          title="Detail Pesanan"
+        >
+          <div className="flex flex-col gap-y-5 p-5 bg-gray-100">
+            {filterData?.map((val) => (
+              <div key={val.id} className="p-4 bg-white rounded-lg shadow-md">
+                <p className="text-lg font-semibold">No order : {val.id}</p>
+                <p className="text-gray-700">
+                  Tanggal Order : {formatedDate(val.createdAt as Date)}
+                </p>
+                <p className="text-gray-700">Customer : {val.customer.name}</p>
+                <p className="text-gray-700">
+                  Invoice Number : {val.invoiceNumber}
+                </p>
+                <p> Nomer Meja : {capitalizeWords(val.place.name)}</p>
+                <p className="mt-1 text-gray-700">
+                  Status :{' '}
+                  <span className="bg-primary text-white p-1 rounded-md">
+                    {val.status === 'delivered'
+                      ? 'Selesai Diproses'
+                      : val.status}
+                  </span>
+                </p>
+
+                <div className="mt-2">
+                  <p className="font-semibold">Pesanan :</p>
+                  {val.orderDetails.map((item, index) => (
+                    <div key={item.product.id} className="flex items-center">
+                      <span className="text-gray-600">
+                        {item.product.name}
+                        {index < val.orderDetails.length - 1},
+                      </span>
+                      <p className="ml-2 text-gray-500">
+                        Jumlah : {item.quantity}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+                <p className="mt-3 font-semibold">Total : {val.amountTotal}</p>
+              </div>
+            ))}
+          </div>
+        </Modal>
+      )}
     </section>
   );
 };
--- libs/web/components/organisms/src/modal/modal.tsx
+++ libs/web/components/organisms/src/modal/modal.tsx
@@ -1,7 +1,7 @@
-import { Icon } from "@iconify/react";
-import { FC, ReactElement, ReactNode, useEffect } from "react";
-import { createPortal } from "react-dom";
-
+import { Icon } from '@iconify/react';
+import { FC, ReactElement, ReactNode, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 export type TModal = {
   isOpen: boolean;
   title?: string;
@@ -28,15 +28,25 @@ export const Modal: FC<TModal> = (props): ReactElement => {
     <div>
       {props?.isOpen &&
         createPortal(
-          <section className="fixed h-screen top-0 left-0 right-0 bottom-0 bg-grey-200/80 bg-blur z-50 flex justify-center items-center">
-            <div className="bg-white transition-all duration-300 ease-in-out w-fit shadow  h-auto">
+          <motion.section
+            initial={{ scale: 0 }}
+            animate={{scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 260,
+              damping: 20,
+            }}
+            className="fixed h-screen top-0 left-0 right-0 bottom-0 bg-grey-200/80 bg-blur z-50 flex justify-center items-center"
+          >
+            <div className="bg-white transition-all duration-300 ease-in-out w-fit shadow h-auto rounded-lg min-w-[28rem]">
               {props.header && (
-                <div className="flex justify-between w-full items-center bg-primary p-4 ">
-                  <h1 className="text-white font-semibold text-2xl">
+                <div className="flex justify-between w-full items-center bg-primary p-4 rounded-t-lg">
+                  <h1 className="text-white font-semibold text-xl">
                     {props.title}
                   </h1>
                   {props.onClose && (
-                    <Icon icon={"ic:baseline-close"}
+                    <Icon
+                      icon={'ic:baseline-close'}
                       data-testid="close-button"
                       className="cursor-pointer text-white font-semibold"
                       onClick={props.onClose}
@@ -55,7 +65,8 @@ export const Modal: FC<TModal> = (props): ReactElement => {
               >
                 {!props.header && (
                   <div className="flex justify-end w-full mb-4">
-                    <Icon icon={"ic:baseline-close"}
+                    <Icon
+                      icon={'ic:baseline-close'}
                       data-testid="close-button"
                       className="cursor-pointer text-grey-400"
                       onClick={props.onClose}
@@ -66,7 +77,7 @@ export const Modal: FC<TModal> = (props): ReactElement => {
                 <div>{props.children}</div>
               </div>
             </div>
-          </section>,
+          </motion.section>,
           document.body
         )}
     </div>
