9a5dcad477536f45b7c70b2f4e4ec178c1f22502
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-21 19:05:42 +0700

    chore: update nx

--- libs/api/src/purchase/controllers/purchase.controller.ts
+++ libs/api/src/purchase/controllers/purchase.controller.ts
@@ -2,7 +2,25 @@ import { router, procedure } from '@fms/trpc-server';
 import { z } from 'zod';
 
 export const purchaseController = router({
-  hello: procedure
+  index: procedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input }) => {
+      return input;
+    }),
+  detail: procedure.input(z.object({ id: z.string() })).query(({ input }) => {
+    return input;
+  }),
+  create: procedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input }) => {
+      return input;
+    }),
+  update: procedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input }) => {
+      return input;
+    }),
+  request: procedure
     .input(z.object({ name: z.string() }))
     .mutation(({ input }) => {
       return input;
