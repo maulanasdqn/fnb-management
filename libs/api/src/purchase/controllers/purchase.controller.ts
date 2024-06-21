1edfeab8eed06512c46fd39b0c21eab0bfb18217
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-22 03:29:34 +0700

    feat: create all service

--- libs/api/src/purchase/controllers/purchase.controller.ts
+++ libs/api/src/purchase/controllers/purchase.controller.ts
@@ -1,28 +1,32 @@
 import { router, procedure } from '@fms/trpc-server';
 import { z } from 'zod';
+import { purchaseService } from '../services/purchase.service';
+import {
+  purchaseApprovalSchema,
+  purchaseCreateSchema,
+  purchaseUpdateSchema,
+  queryParamsSchema,
+} from '@fms/entities';
 
 export const purchaseController = router({
-  index: procedure
-    .input(z.object({ name: z.string() }))
-    .mutation(({ input }) => {
-      return input;
-    }),
-  detail: procedure.input(z.object({ id: z.string() })).query(({ input }) => {
-    return input;
+  index: procedure.input(queryParamsSchema).mutation(({ input }) => {
+    return purchaseService.pagination(input);
   }),
-  create: procedure
-    .input(z.object({ name: z.string() }))
+  detail: procedure.input(z.object({ id: z.string() })).query(({ input }) => {
+    return purchaseService.findOne(input.id);
+  }),
+  create: procedure.input(purchaseCreateSchema).mutation(({ input }) => {
+    return purchaseService.create(input);
+  }),
+  update: procedure.input(purchaseUpdateSchema).mutation(({ input }) => {
+    return purchaseService.update(input);
+  }),
+  requestPurhase: procedure
+    .input(purchaseCreateSchema)
     .mutation(({ input }) => {
-      return input;
+      return purchaseService.requestPurhase(input);
     }),
-  update: procedure
-    .input(z.object({ name: z.string() }))
-    .mutation(({ input }) => {
-      return input;
-    }),
-  request: procedure
-    .input(z.object({ name: z.string() }))
-    .mutation(({ input }) => {
-      return input;
-    }),
+  approval: procedure.input(purchaseApprovalSchema).mutation(({ input }) => {
+    return purchaseService.approval(input);
+  }),
 });
--- libs/entities/src/types/purchase/purchase.type.ts
+++ libs/entities/src/types/purchase/purchase.type.ts
@@ -1,52 +1,63 @@
-import { EPurchaseStatus } from '../../enums/purchase.enum';
-import { TBase, TBaseResponse } from '../common';
+import { z } from 'zod';
+import { baseSchema, TBaseResponse } from '../common';
+import { EPurchaseStatus } from '../../enums';
 
-import { TCommonObject } from '../common';
+export const purchaseDetailCreateOrUpdateSchema = z.object({
+  ingredientId: z.string(),
+  unitTypeId: z.string(),
+  amount: z.number(),
+  price: z.number(),
+});
 
-export type TPurchase = TBase & {
-  amountTotal: number;
-  invoiceNumber: string;
-  supplier: TCommonObject;
-  status: EPurchaseStatus;
-  purchaseDetails: Array<{
-    item: TCommonObject;
-    qty: number;
-    price: number;
-    amount: number;
-  }>;
-};
+export const purchaseCreateSchema = z.object({
+  supplierId: z.string(),
+  details: z.array(purchaseDetailCreateOrUpdateSchema),
+});
 
-export type TPurchaseQueryParams = {
-  id?: string;
-  search?: string;
-};
+export const purchaseUpdateSchema = z.object({
+  id: z.string(),
+  supplierId: z.string().optional(),
+  details: z.array(purchaseDetailCreateOrUpdateSchema),
+});
 
-export type TPurchaseCreateRequest = {
-  amountTotal: number;
-  invoiceNumber: string;
-  supplierId: string;
-  status: EPurchaseStatus;
-  purchaseDetails: Array<{
-    itemId: string;
-    qty: number;
-    price: number;
-    amount: number;
-  }>;
-};
+export const purchaseApprovalSchema = z.object({
+  id: z.string(),
+  status: z.nativeEnum(EPurchaseStatus),
+  rejectionNote: z.string().optional(),
+});
 
-export type TPurchaseUpdateRequest = {
-  id?: string;
-  amountTotal?: number;
-  invoiceNumber?: string;
-  supplierId?: string;
-  status?: EPurchaseStatus;
-  purchaseDetails?: Array<{
-    itemId: string;
-    qty: number;
-    price: number;
-    amount: number;
-  }>;
-};
+export const purchaseDetailSchema = z.object({
+  ingredient: z.object({
+    id: z.string(),
+    name: z.string(),
+  }),
+  unitType: z.object({
+    id: z.string(),
+    name: z.string(),
+  }),
+  price: z.number(),
+  amount: z.number(),
+});
 
+export const purchaseSchema = z.object({
+  supplier: z
+    .object({
+      id: z.string(),
+      fullName: z.string(),
+    })
+    .nullable(),
+  amountTotal: z.number(),
+  invoiceNumber: z.string(),
+  status: z.string(),
+  purchaseDetails: z.array(purchaseDetailSchema),
+  ...baseSchema.shape,
+});
+
+export type TPurchase = z.infer<typeof purchaseSchema>;
+export type TPurchaseCreateRequest = z.infer<typeof purchaseCreateSchema>;
+export type TPurchaseUpdateRequest = z.infer<typeof purchaseUpdateSchema>;
+export type TPurchaseApprovalRequest = z.infer<typeof purchaseApprovalSchema>;
 export type TPurchaseSingleResponse = TBaseResponse<TPurchase>;
-export type TPurchaseResponse = TBaseResponse<TPurchase[]>;
+export type TPurchaseResponse = TBaseResponse<
+  Omit<TPurchase, 'purchaseDetails'>[]
+>;
