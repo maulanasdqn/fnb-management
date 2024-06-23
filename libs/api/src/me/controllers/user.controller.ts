f132f04829e14318f2eae420e3749ba8dbbe1be2
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-23 23:50:21 +0700

    feat: crud user

--- libs/api/src/me/controllers/user.controller.ts
+++ libs/api/src/me/controllers/user.controller.ts
@@ -1,10 +1,29 @@
 import { router, procedure } from '@fms/trpc-server';
 import { z } from 'zod';
 
+import { userService } from '../services/user.service';
+import {
+  queryParamsSchema,
+  userCreateSchema,
+  userUpdateSchema,
+} from '@fms/entities';
+
 export const userController = router({
-  hello: procedure
-    .input(z.object({ name: z.string() }))
+  index: procedure.input(queryParamsSchema).query(({ input }) => {
+    return userService.pagination(input);
+  }),
+  detail: procedure.input(z.object({ id: z.string() })).query(({ input }) => {
+    return userService.detail(input.id);
+  }),
+  create: procedure.input(userCreateSchema).mutation(({ input }) => {
+    return userService.create(input);
+  }),
+  update: procedure.input(userUpdateSchema).mutation(({ input }) => {
+    return userService.update(input);
+  }),
+  delete: procedure
+    .input(z.object({ id: z.string() }))
     .mutation(({ input }) => {
-      return input;
+      return userService.delete(input.id);
     }),
 });
--- libs/entities/src/types/me/user.type.ts
+++ libs/entities/src/types/me/user.type.ts
@@ -1,14 +1,13 @@
 import { z } from 'zod';
 import { baseSchema, TBaseResponse } from '../common';
-import { roleSchema } from './role.type';
 
 export const userSchema = z.object({
+  id: z.string(),
   fullname: z.string(),
-  username: z.string().optional(),
-  avatar: z.string().optional(),
-  password: z.string().optional(),
-  role: roleSchema.optional(),
-  ...baseSchema.shape,
+  username: z.string(),
+  avatar: z.string().nullable(),
+  role: z.object({ id: z.string(), name: z.string() }).nullable(),
+  ...baseSchema.omit({ id: true }).shape,
 });
 
 export const userCreateSchema = z.object({
@@ -31,5 +30,5 @@ export const userUpdateSchema = z.object({
 export type TUser = z.infer<typeof userSchema>;
 export type TUserCreateRequest = z.infer<typeof userCreateSchema>;
 export type TUserUpdateRequest = z.infer<typeof userUpdateSchema>;
-export type TUserResponse = TBaseResponse<TUser>;
-export type TUserSingleResponse = TBaseResponse<TUser[]>;
+export type TUserResponse = TBaseResponse<Omit<TUser, 'avatar'>[]>;
+export type TUserSingleResponse = TBaseResponse<TUser>;
