ae73a441764e3a2af40aa66aa2571e1503dd4d30
Mohammad Nurfahmi Sugiarto <92211447+sugiartofahmi@users.noreply.github.com>
2024-06-24 19:32:17 +0700

    feat: crud supplier (#69)
    * chore: unused option
    
    * chore: update nx
    
    * feat: create all service
    
    * fix: wrong query
    
    * feat: delete service
    
    * chore: github action running after sync
    
    * chore: github action running after sync
    
    * chore: seeder supplier
    
    * feat: crud supplier

--- libs/entities/src/types/supplier/supplier.type.ts
+++ libs/entities/src/types/supplier/supplier.type.ts
@@ -1,22 +1,22 @@
 import { z } from 'zod';
-import { baseSchema, TBase, TBaseResponse } from '../common';
+import { baseSchema, TBaseResponse } from '../common';
 
 export const supplierSchema = z.object({
-  fullname: z.string(),
+  fullName: z.string(),
   address: z.string(),
   phoneNumber: z.string(),
   ...baseSchema.shape,
 });
 
 export const supplierCreateSchema = z.object({
-  fullname: z.string(),
+  fullName: z.string(),
   address: z.string(),
   phoneNumber: z.string(),
 });
 
 export const supplierUpdateSchema = z.object({
   id: z.string(),
-  fullname: z.string().optional(),
+  fullName: z.string().optional(),
   address: z.string().optional(),
   phoneNumber: z.string().optional(),
 });
