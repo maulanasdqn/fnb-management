5c8bb6c9518b5e8d2ce66c5a31d8f79b288ce5a3
sugiartofahmi <sugiartofahmi@gmail.com>
2024-06-27 00:19:07 +0700

    improve: crud product

--- libs/entities/src/types/product/product.type.ts
+++ libs/entities/src/types/product/product.type.ts
@@ -1,8 +1,10 @@
 import { baseSchema, TBaseResponse } from '../common';
 import { z } from 'zod';
 import { variantProductSchema } from '../variant';
+import { recipeIngredientSchema, recipeSchema } from '../recipe';
 
 export const productSchema = z.object({
+  id: z.string(),
   name: z.string(),
   priceSelling: z.number(),
   image: z.string().optional().nullable(),
@@ -15,44 +17,33 @@ export const productSchema = z.object({
     })
     .optional()
     .nullable(),
-  ingredients: z
-    .array(
-      z.object({
-        id: z.string(),
-        name: z.string(),
-        amount: z.number(),
-        unitType: z.object({
-          id: z.string(),
-          name: z.string(),
-        }),
-      })
-    )
-    .optional(),
-  ...baseSchema.shape,
+  recipe: recipeSchema.optional().nullable(),
+  ...baseSchema.omit({ id: true }).shape,
 });
 
 export const productCreateSchema = z.object({
   name: z.string(),
   priceSelling: z.number(),
-  productCategoryId: z.string().optional(),
+  productCategoryId: z.string(),
+  recipeId: z.string(),
   image: z.string().nullable(),
   description: z.string().optional().nullable(),
-  ingredientId: z.string().optional(),
-  ...baseSchema.shape,
 });
 
 export const productUpdateSchema = z.object({
+  id: z.string(),
   name: z.string().optional(),
   priceSelling: z.number().optional(),
   productCategoryId: z.string().optional(),
   image: z.string().optional().nullable(),
   description: z.string().optional().nullable(),
-  ingredientId: z.string().optional(),
-  ...baseSchema.shape,
+  recipeId: z.string().optional(),
 });
 
 export type TProduct = z.infer<typeof productSchema>;
 export type TProductSingleResponse = TBaseResponse<TProduct>;
-export type TProductResponse = TBaseResponse<TProduct[]>;
+export type TProductResponse = TBaseResponse<
+  Omit<TProduct, 'recipe' | 'variants'>[]
+>;
 export type TProductCreateRequest = z.infer<typeof productCreateSchema>;
 export type TProductUpdateRequest = z.infer<typeof productUpdateSchema>;
